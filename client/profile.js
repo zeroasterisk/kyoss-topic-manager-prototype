512b67a94191d311ac2f6d6b7ac34fe3a46fa95e
alan bount <alan@zeroasterisk.com>
2014-08-08 09:44:58 -0400

    fix for login buttons

--- client/profile.js
+++ client/profile.js
@@ -14,3 +14,15 @@ Template.profile.helpers({
     return Meeting.getUserVotesAllowed(mtg, userId);
   }
 });
+
+// hedaer login button taking people to the Profile
+// https://github.com/mangasocial/meteor-accounts-ui-bootstrap-3/
+Template._loginButtonsLoggedInDropdown.events({
+  'click #login-buttons-edit-profile': function(event) {
+    event.stopPropagation();
+    Template._loginButtons.toggleDropdown();
+    Router.go('profile');
+  }
+});
+
+
